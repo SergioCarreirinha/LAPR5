7a523a1ab8adf8572714e77b980735714a106fc3
1181844 <1181844@isep.ipp.pt>
2020-11-13 17:23:05 +0000

    UC06 - tests

--- myExpressApp/tests/model/VehicleType.test.ts
+++ myExpressApp/tests/model/VehicleType.test.ts
@@ -2,7 +2,7 @@ import {VehicleType} from '../../src/domain/models/VehicleType';
 import IVehicleTypeDTO from '../../src/dto/VehicleTypeDTO/IVehicleTypeDTO';
 import {expect} from 'chai';
 
-describe('Create a valid Driver Type', () => {
+describe('Create a valid Vehicle Type', () => {
 
     let vehicleType = VehicleType.create({ 
         key: "Teste",
@@ -20,11 +20,133 @@ describe('Create a valid Driver Type', () => {
     });
 });
 
-describe('Create a invalid Driver Type', () => {
+describe('Create a invalid Vehicle Type', () => {
 
     let vehicleType = VehicleType.create({key: ""} as IVehicleTypeDTO);
 
     it("ensure all Parameters are well formed", () => {
         expect(vehicleType.error).to.equal("Must provide a key");
+    });
+});
+
+
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({
+        key: "Teste",
+        name: "",
+        autonomy: -30,
+        cost: 1234,
+        averageSpeed: 21,
+        energySource: 12,
+        consumption: 12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a name");
+    });
+});
+
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: -30,
+        cost: 1234,
+        averageSpeed: 21,
+        energySource: 12,
+        consumption: 12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid autonomy");
+    });
+});
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: 30,
+        cost: -1234,
+        averageSpeed: 21,
+        energySource: 12,
+        consumption: 12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid cost");
+    });
+});
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: 30,
+        cost: 1234,
+        averageSpeed: -21,
+        energySource: 12,
+        consumption: 12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid averageSpeed");
+    });
+});
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: 30,
+        cost: 1234,
+        averageSpeed: 21,
+        energySource: -12,
+        consumption: 12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid energySource");
+    });
+});
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: 30,
+        cost: 1234,
+        averageSpeed: 21,
+        energySource: 12,
+        consumption: -12,
+        emissions: 12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid consumption");
+    });
+});
+describe('Create a invalid Vehicle Type', () => {
+
+    let vehicleType = VehicleType.create({ 
+        key: "Teste",
+        name: "Autocarro",
+        autonomy: 30,
+        cost: 1234,
+        averageSpeed: 21,
+        energySource: 12,
+        consumption: 12,
+        emissions: -12
+    } as IVehicleTypeDTO);
+
+    it("ensure all Parameters are well formed", () => {
+        expect(vehicleType.error).to.equal("Must provide a valid emission rate");
     });
 });
