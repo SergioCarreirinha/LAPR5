eab6981d7b80ec68c04f07c6b88422b353b349eb
José Cunha <1181494@isep.ipp.pt>
2020-11-14 22:23:01 +0000

    UC05 - mock testing

---
+++ myExpressApp/src/controllers/test/DriverTypeController.test.ts
@@ -0,0 +1,45 @@
+import * as sinon from 'sinon';
+import { Response, Request, NextFunction } from 'express';
+
+import { Container } from 'typedi';
+import config from "../../config";
+
+import { Result } from '../../core/logic/Result';
+
+import IDriverTypeDTO from '../../dto/DriverTypeDTO/IDriverTypeDTO';
+import DriverTypeController from '../../controllers/DriverTypeController';
+import IDriverTypeService from '../../services/interface/IDriverTypeService';
+import { DriverType } from '../../domain/models/DriverType';
+
+describe('Line Controller', function () {
+	beforeEach(function() {
+    });
+
+    afterEach(function() { 
+    });
+
+    it('createDriverType: returns json with DriverType values', async function() {
+        let body = {"description": "teste"};
+        let req: Partial<Request> = {};
+		req.body = body;
+
+        let res: Partial<Response> = {
+			json: sinon.spy()
+        };
+		let next: Partial<NextFunction> = () => {};
+
+		let driverTypeServiceClass = require('../'+config.services.DriverType.path).default;
+		let driverTypeServiceInstance = Container.get(driverTypeServiceClass)
+		Container.set(config.services.DriverType.name, driverTypeServiceInstance);
+
+		driverTypeServiceInstance = Container.get(config.services.DriverType.name);
+        const mock = sinon.stub(driverTypeServiceInstance, "createDriverType").returns(Result.ok<IDriverTypeDTO>({"description": "teste"}));
+		const ctrl = new DriverTypeController(driverTypeServiceInstance as IDriverTypeService);
+
+        await ctrl.createDriverType(<Request>req, <Response>res, <NextFunction>next);
+
+		sinon.assert.calledOnce(mock);
+        sinon.assert.calledWith(mock, sinon.match({"description": "teste"}));
+        mock.restore();
+    });
+});
