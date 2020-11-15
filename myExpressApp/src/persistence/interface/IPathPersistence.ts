a43806f2c91e86e0537e5d20933f162afff01df7
José Cunha <1181494@isep.ipp.pt>
2020-11-15 15:31:37 +0000

    UC01- Paths importing

--- myExpressApp/src/persistence/interface/IPathPersistence.ts
+++ myExpressApp/src/persistence/interface/IPathPersistence.ts
@@ -1,6 +1,7 @@
 import { PathNode } from '../../domain/models/PathNode';
 
 export interface IPathPersistence {
+    domainId: string,
     key: string,
     isEmpty: boolean,
     pathNodes: PathNode[],
