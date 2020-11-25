fb8ebadf98506c0b6b1c3b8c13b40574c329ac76
1181844 <1181844@isep.ipp.pt>
2020-11-25 15:43:31 +0000

    Nodes connection to SPA

--- MasterDataRede/src/services/interface/INodeService.ts
+++ MasterDataRede/src/services/interface/INodeService.ts
@@ -6,5 +6,5 @@ import { Request } from 'express';
 export default interface INodeService {
     createNode(nodeDTO : INodeDTO): Promise<Result<INodeDTO>>;
     findByName(value: string): Promise<Result<Node>>;
-    findAll(req: Request): Promise<Result<Array<Node>>>;
+    findAll(req: Request): Promise<Result<Array<INodeDTO>>>;
 }
