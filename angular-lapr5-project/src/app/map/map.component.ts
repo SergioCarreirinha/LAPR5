a1a57d6d7de021d8a870345b59b84fc82690b61f
José Cunha <1181494@isep.ipp.pt>
2020-12-02 11:48:09 +0000

    UC20 - implementation

--- angular-lapr5-project/src/app/map/map.component.ts
+++ angular-lapr5-project/src/app/map/map.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { GeoCoordinates } from '@here/harp-geoutils';
+import { OmvTileDecoder } from "@here/harp-omv-datasource/index-worker";
+import { MapAnchor, MapView } from "@here/harp-mapview";
+import { APIFormat, AuthenticationMethod, OmvDataSource } from "@here/harp-omv-datasource";
+import { MapControls } from '@here/harp-map-controls';
 import * as THREE from 'three';
-import * as Mappa from 'mappa-mundi';
+import { NodeService } from '../services/node.service';
+import { INode } from '../interfaces/INode';
+import { RGBADepthPacking } from 'three';
 
 @Component({
   selector: 'app-map',
@@ -8,37 +15,86 @@ import * as Mappa from 'mappa-mundi';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
+  nodes: INode[] = [];
+  mapView: MapView;
+  mapControls: MapControls;
+  constructor(private service: NodeService) {
+    
+  }
 
-  public constructor() { }
-
-  public ngOnInit() { 
+  ngOnInit() {
     this.createMap();
   }
 
-
   createMap(){
-    
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera();
-    const canvas = document.getElementById("map") as HTMLCanvasElement;
-    const renderer = new THREE.WebGLRenderer({ alpha: true, canvas: canvas });
-    const key = "pk.eyJ1IjoiY3VuaGFhcyIsImEiOiJja2k0eXFsaDEwMXFvMnJucTlzOTE4bjk3In0.1BWcHMvsWG8bpgOSa2LNng";
-    const options = {
-      lat: 41.14961, 
-      lng: -8.61099,
-      zoom: 13,
-    }
-    scene.add(camera);
-    renderer.setSize(window.innerWidth-60, window.innerHeight-150);
-    // Light
-    const light = new THREE.PointLight(0xffffff, 1.2);
-    light.position.set(0, 0, 6);
-    scene.add(light);
+    const canvas = document.getElementById('map') as HTMLCanvasElement;
+    this.mapView = new MapView({
+      canvas,
+      theme:
+        'https://unpkg.com/@here/harp-map-theme/resources/berlin_tilezen_base.json',
+      maxVisibleDataSourceTiles: 40,
+      tileCacheSize: 100
+    });
 
-    const mappa = new Mappa('MapboxGL', key);
-    const myMap = mappa.tileMap(options);
-    myMap.overlay(canvas);
-    
+    this.mapControls = new MapControls(this.mapView);
+
+    const startLocation = new GeoCoordinates(41.187208, -8.375702);
+    this.mapView.lookAt({ target: startLocation, zoomLevel: 11.5 });
+
+    this.mapView.resize(window.innerWidth-60, window.innerHeight-150);
+    window.addEventListener('resize', () => {
+      this.mapView.resize(window.innerWidth-60, window.innerHeight-150);
+    });
+
+    const omvDataSource = new OmvDataSource({
+      apiFormat: APIFormat.XYZOMV,
+      styleSetName: "tilezen",
+  
+      baseUrl: "https://vector.hereapi.com/v2/vectortiles/base/mc",
+      authenticationCode: "tG0O7q7DN0IW9BjSznkxfInoA_EjFKr8Sxx4m8TEPEs",
+      authenticationMethod: {
+          method: AuthenticationMethod.QueryString,
+          name: "apikey"
+      },
+      decoder: new OmvTileDecoder()
+    });
+  
+    this.mapView.addDataSource(omvDataSource);
+    this.service.getNodes().subscribe(node => {
+      this.nodes = node;
+      console.log(this.nodes);
+      for(let i = 0; i< this.nodes.length; i++) {
+        const geoPosition = new GeoCoordinates(this.nodes[i].latitude, this.nodes[i].longitude);
+        if (geoPosition === null) {
+            return;
+        }
+        
+        const cube = this.createPoint();
+        cube.anchor = geoPosition;
+        this.mapView.mapAnchors.add(cube);
+        this.mapView.update();
+      }
+    });
   }
 
+  createPoint(): MapAnchor<THREE.Object3D>{
+    const cube = new THREE.Object3D();
+    const geometry = new THREE.CircleGeometry(350, 30);
+    const material = new THREE.MeshStandardMaterial({ color: "rgb(255,0,0)"});
+    const prePassMaterial = new THREE.MeshStandardMaterial({
+      color: "rgb(255,0,0)",
+      opacity: 0.3,
+      depthTest: false,
+      transparent: true
+    });
+    const prePassMesh = new THREE.Mesh(geometry, prePassMaterial);
+    prePassMesh.renderOrder = Number.MAX_SAFE_INTEGER - 1;
+    cube.add(prePassMesh);
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.renderOrder = Number.MAX_SAFE_INTEGER;
+    cube.add(mesh);
+    return cube;
+  }
+  
 }
