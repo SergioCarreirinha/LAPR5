a1e247e04024912c11e53e6ada6da3453c7dc193
José Cunha <1181494@isep.ipp.pt>
2020-12-11 12:29:40 +0000

    MDV - setup

--- MasterDataViagem/Startup.cs
+++ MasterDataViagem/Startup.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using TodoApi.Models;
 
 namespace MasterDataViagem
 {
@@ -25,6 +27,8 @@ namespace MasterDataViagem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt =>
+                opt.UseInMemoryDatabase("TodoList"));
             services.AddControllers();
         }
 
