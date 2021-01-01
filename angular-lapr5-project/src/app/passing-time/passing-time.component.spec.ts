c158d1a0d226f42864401aeb23f47d10a40a5ee1
José Cunha <1181494@isep.ipp.pt>
2021-01-01 19:43:47 +0000

    C.7 - fixing errors on spa passing time component test

--- angular-lapr5-project/src/app/passing-time/passing-time.component.spec.ts
+++ angular-lapr5-project/src/app/passing-time/passing-time.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PassingTimeComponent } from './passing-time.component';
@@ -8,7 +9,8 @@ describe('PassingTimeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PassingTimeComponent ]
+      declarations: [ PassingTimeComponent ],
+      imports: [HttpClientTestingModule]
     })
     .compileComponents();
   });
