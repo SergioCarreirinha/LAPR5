273231fd8921ce296a0c79ca0c5d2484cd897ceb
José Cunha <1181494@isep.ipp.pt>
2021-01-02 18:38:18 +0000

    Relatorio de asist

--- angular-lapr5-project/src/environments/environment.prod.ts
+++ angular-lapr5-project/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {accesstoken:'pk.eyJ1IjoiY3VuaGFhcyIsImEiOiJja2k0eXFsaDEwMXFvMnJucTlzOTE4bjk3In0.1BWcHMvsWG8bpgOSa2LNng'},
+  url: {mdr: 'https://mdr25.azurewebsites.net/', mdv:'https://mdv-g25.azurewebsites.net/'},
 };
