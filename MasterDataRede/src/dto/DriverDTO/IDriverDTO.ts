47c546fe036914b872be1f689a5bcdfe5a530878
1181844 <1181844@isep.ipp.pt>
2021-01-02 14:17:08 +0000

    MDR Driver correction

--- MasterDataRede/src/dto/DriverDTO/IDriverDTO.ts
+++
@@ -1,9 +0,0 @@
-import { DriverType } from "../../domain/models/DriverType";
-
-export default interface IDriverDTO {
-    name: string;
-    birthdate: Date;
-    driverLicenseNum: Number;
-    licenseExpiration: Date;
-    driverTypes: Array<DriverType>;
-}
--- MasterDataRede/src/repositories/interface/IDriverRepo.ts
+++
@@ -1,5 +0,0 @@
-import { Driver } from "../../domain/models/Driver";
-
-export default interface IDriverRepo{
-    save(Driver: Driver): Promise<Driver>
-}
