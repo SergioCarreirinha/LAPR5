356c85cf5a2a33dec63b3322e37a48e75d1a8764
1181844 <1181844@isep.ipp.pt>
2021-01-05 23:42:46 +0000

    Driver Validation on html SPA

--- angular-lapr5-project/src/app/driver/driver.component.ts
+++ angular-lapr5-project/src/app/driver/driver.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { IDriver } from '../interfaces/IDriver';
 import { DriverService } from '../services/driver.service';
 
@@ -15,11 +16,30 @@ export class DriverComponent implements OnInit {
   }
 
   createDriver(name: string, birthdate: string, driverLicenseNum: string, licenseExpiration: string){
+  if(birthdate != '' && name != '' && driverLicenseNum !='' && licenseExpiration != '' ){
     this.driverService.createDriver({
       name: name,
       birthdate: birthdate,
       driverLicenseNum: parseInt(driverLicenseNum),
       licenseExpiration: licenseExpiration
     } as IDriver).subscribe()
+    Swal.fire({
+      title: 'Success!',
+      text: 'Driver Created',
+      icon: 'success',
+      confirmButtonText: 'Ok',
+      timer: 2500,
+      showConfirmButton: true,
+    })
+   }else{
+    Swal.fire({
+      title: 'Error!',
+      text: 'Verify Fields!',
+      icon: 'error',
+      confirmButtonText: 'Ok',
+      timer: 2500,
+      showConfirmButton: true,
+    })
+   }
   }
 }
