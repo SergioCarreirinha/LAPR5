9986b7e9279f04eae68a858621f606f4cce989ea
José Cunha <1181494@isep.ipp.pt>
2021-01-05 18:10:45 +0000

    Server prolog deploy

--- Planeamento/SprintC/server.pl
+++ Planeamento/SprintC/server.pl
@@ -5,31 +5,35 @@
       [ server/1            % ?Port
       ]).
 
+:- use_module(library(http/http_json)).
+:- use_module(library(http/http_open)).
+:- use_module(library(http/json)).
+:- use_module(library(http/json_convert)).
+:- use_module(library(http/http_client)).
+:- use_module(library(http/http_parameters)).
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_open)).
-:- use_module(library(http/http_client)).
-:- use_module(library(http/json)).
-:- use_module(library(http/http_json)).
-:- use_module(library(http/http_cors)).
-:- use_module(library(http/json_convert)).
-:- set_setting(http:cors, [*]).
+:- use_module(library(http/http_files)).
+:- use_module(library(http/http_unix_daemon)).
+:- use_module(library(http/http_log)).
+:- use_module(library(http/http_dyn_workers)).
+:- http_handler(root(.), http_reply_from_files('.', []), [prefix]).
 
 server(Port) :-
     http_server(http_dispatch,
                 [ port(Port),
                   workers(16)
                 ]).
-                
+
+:- http_handler('api/genetics',post_data,[]).
+
 server(Port) :-
         http_server(http_dispatch, [ port(Port), workers(16) ]).
 
-:- http_handler(root(api), handle_request, []).
-:- http_handler(root(api/genetics),inicialize,[]).
 
 
 
-inicialize(Request):-
+post_data(Request):-
 	(
 		(
 			(option(methods(options),Request),!,
